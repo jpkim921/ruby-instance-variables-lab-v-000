01b0aaa378b08c08399f85cd489f9769961ff615
Jayon Kim <jpkim921@gmail.com>
2018-01-27 16:37:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,10 @@
 class Dog
+  # The first method takes in an argument of a dog's name and sets that argument equal to a variable
   def name=(dog_name)
     this_dogs_name = dog_name
   end
 
+  # second method is responsible for reporting, or reading the name
   def name
     this_dogs_name
   end
